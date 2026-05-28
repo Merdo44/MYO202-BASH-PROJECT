ad260f03f71678ca615156af535b383f9ff99dbf
Mert Ali YILMAZ <mertali99@outlook.com>
2026-05-28 03:50:07 +0300

    İlk yükleme

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# İsim Soyisim: Mert Ali YILMAZ
+# Öğrenci Numarası: 2420171021
+# Sertifika Bağlantıları: Docker Temelleri: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=bx1hL8oPEw
+#                         Siber Güvenlikte Linux İşletim Sistemleri: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=Yx1h8DWmem
+#                         Linux Bash Script Eğitimi: 
+
+date '+%Y-%m-%dT%H:%M:%S' > report.log
+
+echo "Islemci: 13th Gen Intel(R) Core(TM) i7-13700K" >> report.log
+echo "Ram: 32 GB" >> report.log
+echo "Anakart: PRO Z790-P WIFI DDR4 (MS-7E06)" >> report.log
+echo "UUID: DAB99063-0A8E-4A19-A6FC-047C164D13A4" >> report.log
+echo "MAC Adresi: 04-7C-16-4D-13-A4" >> report.log
+
+read -s -p "Parola girin: " girilen_sife
+echo ""
+
+PAROLA="${girilen_sifre}MYO+202"
+
+gpg --batch --yes --cipher-algo AES256 --passphrase "$PAROLA" -c report.log
+
+rm report.log
