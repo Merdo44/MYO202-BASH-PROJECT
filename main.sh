c6b5b7485f1ab3f43f94034a4588ac79c7c56761
Mert Ali YILMAZ <mertali99@outlook.com>
2026-05-30 18:16:33 +0300

    disk model, disk boyut ve ram kapesitesi boyutlar gb cinsinden olacak şekilde düzenlendi

--- main.sh
+++ main.sh
@@ -11,7 +11,7 @@ Islemci=$(wmic cpu get name | sed -n '2p' | tr -d '\r' | xargs)
 echo "Islemci: $Islemci" >> report.log
 
 Ram_Kapasitesi=$(wmic computersystem get totalphysicalmemory | sed -n '2p' | tr -d '\r' | xargs)
-echo "Ram: $Ram_Kapasitesi" >> report.log
+echo "Ram: $((Ram_Kapasitesi / 1024 / 1024 / 1024))GB" >> report.log
 
 Anakart=$(wmic baseboard get product | sed -n '2p' | tr -d '\r' | xargs)
 echo "Anakart: $Anakart" >> report.log
@@ -19,8 +19,9 @@ echo "Anakart: $Anakart" >> report.log
 Anakart_UUID=$(wmic csproduct get uuid | sed -n '2p' | tr -d '\r' | xargs)
 echo "Anakart UUID: $Anakart_UUID" >> report.log
 
-Disk_Bilgisi=$(wmic diskdrive get model, serialnumber, size | sed -n '2p' | tr -d '\r' | xargs)
-echo "Disk Bilgileri (Model - Seri No - Boyut): $Disk_Bilgisi" >> report.log
+Disk_Model=$(wmic diskdrive get model, serialnumber | sed -n '2p' | tr -d '\r' | xargs)
+Disk_Boyutu=$(wmic diskdrive get size | sed -n '2p' | tr -d '\r' | xargs)
+echo "Disk Bilgileri (Model-Seri No-Boyut): $Disk_Model $((Disk_Boyutu / 1024 / 1024 / 1024))GB" >> report.log
 
 Mac_Adresi=$(getmac | head -n 4 | tail -n 1 | awk '{print $1}' | tr -d '\r')
 echo "MAC Adresi: $Mac_Adresi" >> report.log
