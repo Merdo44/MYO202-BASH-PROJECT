60a928a77ca48e0d34b040a0eeddeba55da0203b
Mert Ali YILMAZ <mertali99@outlook.com>
2026-06-01 14:49:55 +0300

    güncelleme yapıldı

--- main.sh
+++ main.sh
@@ -8,30 +8,25 @@
 date '+%Y-%m-%dT%H:%M:%S' > report.log
 
 Islemci=$(wmic cpu get name | sed -n '2p' | tr -d '\r' | xargs)
-echo "Islemci: $Islemci" >> report.log
+echo "Islemci - Marka Model: $Islemci" >> report.log
 
-Ram_Kapasitesi=$(wmic computersystem get totalphysicalmemory | sed -n '2p' | tr -d '\r' | xargs)
-echo "Ram: $((Ram_Kapasitesi / 1024 / 1024 / 1024))GB" >> report.log
+Ram_Detay=$(wmic memorychip get manufacturer,model,partnumber,serialnumber,capacity | sed -n '2p' | tr -d '\r' | xargs)
+echo "RAM - Uretici/Model/ParcaNo/SeriNo/Kapasite: $Ram_Detay" >> report.log
 
-Anakart=$(wmic baseboard get product | sed -n '2p' | tr -d '\r' | xargs)
-echo "Anakart: $Anakart" >> report.log
+Anakart_Detay=$(wmic baseboard get manufacturer,product,serialnumber | sed -n '2p' | tr -d '\r' | xargs)
+echo "Anakart - Uretici/Model/SeriNo: $Anakart_Detay" >> report.log
 
 Anakart_UUID=$(wmic csproduct get uuid | sed -n '2p' | tr -d '\r' | xargs)
 echo "Anakart UUID: $Anakart_UUID" >> report.log
 
-Disk_Model=$(wmic diskdrive get model, serialnumber | sed -n '2p' | tr -d '\r' | xargs)
-Disk_Boyutu=$(wmic diskdrive get size | sed -n '2p' | tr -d '\r' | xargs)
-echo "Disk Bilgileri (Model-Seri No-Boyut): $Disk_Model $((Disk_Boyutu / 1024 / 1024 / 1024))GB" >> report.log
+Disk_Bilgisi=$(wmic diskdrive get manufacturer,model,serialnumber,size | sed -n '2p' | tr -d '\r' | xargs)
+echo "Disk - Marka/Model/SeriNo/Kapasite: $Disk_Bilgisi" >> report.log
 
 Mac_Adresi=$(getmac | head -n 4 | tail -n 1 | awk '{print $1}' | tr -d '\r')
 echo "MAC Adresi: $Mac_Adresi" >> report.log
 
 
-read -s -p "Parola giriniz: " girilen_sifre
-echo ""
-
-PAROLA="${girilen_sifre}MYO+202"
+PAROLA="MYO+202"
 
 gpg --batch --yes --cipher-algo AES256 --passphrase "$PAROLA" -c report.log
-
 rm report.log
